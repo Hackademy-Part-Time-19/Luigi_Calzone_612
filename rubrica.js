660040d97e74bbc7205e98f81cd85062ad9078f5
Luigi.Calzone <luigi.calzone.19@gmail.com>
2023-12-05 21:46:18 +0100

    esercizio 6 12 2023

---
+++ rubrica.js
@@ -0,0 +1,6 @@
+function apriModale() {
+    document.getElementById("modale").style.display = "flex";
+}
+function chiudiModale() {
+    document.getElementById("modale").style.display = "none";
+}
